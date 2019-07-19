eab1f71b6e1d96ea64f68f320d4708f3f800a0c7
Sellieee <sellieee@hotmail.com>
2019-07-19 22:57:51 +1000

    theme song added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,29 @@
-// Hiding the quiz until user clicks on "Start Quiz!"
-$(function () {
-    $(".quiz-start").hide();
-});
+$(document).ready(function () {
 
-// Start button
-$(".button-start").click(function () {
-    $(".quiz-start").show();
-})
+    // Hiding the quiz until user clicks on "Start Quiz!"
+    $(function () {
+        $(".quiz-start").hide();
+    });
 
-// Restart button
-$(".button-restart").click(function () {
-    window.location.reload();
+    // Playing audio when "Start Quiz!" button is clicked
+    const sound = new Audio();
+    document.getElementById("start").addEventListener("click", playSound);
+
+    function playSound() {
+        sound.src = "./assets/theme-song.mp3";
+        sound.play();
+    }
+
+
+    // Start button and plays music
+    $(".button-start").click(function () {
+
+        $(".quiz-start").show();
+        $(".button-start").hide();
+    })
+
+    // Restart button
+    $(".button-restart").click(function () {
+        window.location.reload();
+    })
 })
